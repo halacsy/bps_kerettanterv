f6744dc2711244948129ee1036d8da3e8e4f4cd3
Peter Halacsy <hp@budapestschool.org>
2019-07-11 19:09:25 +0200

    new learning outcome csv introduced. From this point this csv file is the single source of truth

--- code/convert_lo.py
+++ code/convert_lo.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import csv
 import re
-import jinja2
+
 
 map = {}
 translationFile = "/Users/bp268/Documents/Code/kerettanterv/traslations.csv"
@@ -56,13 +56,23 @@ with open('lo.csv') as csv_file:
                 lo = map[lo]
 
             else:
-                print("% no translations found", lo)
+                pass
 
             # del jelzi, hogy torolni kell.
             if len(lo) < 10:
                 continue
-
-            table.append({'semester': semester,
+            if "három mesetípust" in lo:
+                los = lo.split('.')
+                los = [lo.strip() + "." for lo in los if len(lo)>4]
+                for lo in los:
+                    table.append({'semester': semester,
+                          'period': row[1],
+                          'subject': row[2],
+                          'orig_subject': map_orig_subjects(row[3]),
+                          'lo': lo,
+                          })
+            else:
+                table.append({'semester': semester,
                           'period': row[1],
                           'subject': row[2],
                           'orig_subject': map_orig_subjects(row[3]),
@@ -70,76 +80,5 @@ with open('lo.csv') as csv_file:
                           })
 
 
-
-periods = ["1-2", "3-4", "5-6", "7-8", "9-10", "11-12"]
-semesters = [0,1, 2, 3, 4]
-integrated_subjects = ['STEM', 'KULT', 'Harmónia']
-kiemelt_subject = ['Magyar nyelv és irodalom'
-                   ,'Történelem, társadalmi és állampolgári ismeretek', 'Matematika', "Testnevelés és sport", "Idegen nyelv"]
-
-
-
-def select(semester, period, kiemelt_subject):
-    return [lo for lo in table if lo['semester'] == semester and lo['period'] == period and lo['orig_subject'] == kiemelt_subject]
-
-
-def collect_high_priority_subject_lo(subject):
-    result = []
-    for p in periods:
-
-        r_semesters = []
-        for s in semesters:
-            r_semesters.append({"semester": s,
-                              "los": select(s, p, subject)
-            })
-            
-        result.append({'years': p, "semesters": r_semesters})
-
-    return {'name': subject, 'periods': result}
-
-def collect_integrated_subject_lo(subject):
-    result = []
-    for p in periods:
-
-        r_semesters = []
-        for s in semesters:
-            cucc =  [lo for lo in table if lo['semester'] == s and lo['period'] == p and lo['subject'] == subject]
-
-            r_semesters.append({"semester": s,
-                              "los": cucc
-            })
-            
-        result.append({'years': p, "semesters": r_semesters})
-
-    return {'name': subject, 'periods': result}
-
-curriculum = {
-    'high_priority_subjects': [],
-    'integrated_subjects': []
-}
-
-curriculum['high_priority_subjects'] = [ collect_high_priority_subject_lo(s) for s in kiemelt_subject]
-curriculum['integrated_subjects']= [ collect_integrated_subject_lo(s) for s in integrated_subjects]
-
-templateLoader = jinja2.FileSystemLoader(searchpath="./")
-templateEnv = jinja2.Environment(loader=templateLoader)
-
-latex_jinja_env = jinja2.Environment(
-    block_start_string='\BLOCK{',
-    block_end_string='}',
-    variable_start_string='\VAR{',
-    variable_end_string='}',
-    comment_start_string='\#{',
-    comment_end_string='}',
-    line_statement_prefix='%%',
-    line_comment_prefix='%#',
-    trim_blocks=True,
-    autoescape=False,
-    loader=templateLoader)
-
-TEMPLATE_FILE = "chapters/kerettanterv/eredmenyek-template.tex"
-template = latex_jinja_env.get_template(TEMPLATE_FILE)
-# this is where to put args to the template renderer
-outputText = template.render(curriculum=curriculum)
-
-print(outputText)
+import json
+print(json.dumps({'learning_outcomes': table}, sort_keys=True, indent=4, ensure_ascii=False))
